e2d2af89f9727e096258bfdec1a7b42b97250264
TH3_S1LENC3 <54558771+Th3-S1lenc3@users.noreply.github.com>
2022-02-22 18:12:58 +0000

    Add setup-server

---
+++ setup-server/files/.bashrc
@@ -0,0 +1,116 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+unset HISTFILE
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=100
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+#[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    GREEN="\[\033[0;32m\]"
+    RED="\[\033[1;31m\]"
+    BLUE="\[\033[1;34m\]"
+    WHITE="\[\033[0;37m\]"
+    PS1="${GREEN}┌──${debian_chroot:+($debian_chroot)──}[${RED}\u@\h${GREEN}]-[${BLUE}\w${GREEN}]\n${GREEN}└╼${BLUE}\$${WHITE} "
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls -al --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+else
+    alias ls='ls -al'
+fi
+
+# some more ls aliases
+alias l='ls -CF'
+
+alias cp='advcp -g'
+alias mv='advmv -g'
+
+# Some more alias to avoid making mistakes:
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.aliases ]; then
+    . ~/.aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+export PATH=/bin/lscript:/bin/lscript:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
---
+++ setup-server/stackscript.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+# Version 0.1.0
+
+# USER CONFIG
+#
+# <UDF name="user_name" label="Non-Root User Name" default="administrator" />
+# <UDF name="user_password" label="Non-Root User Password"/>
+# <UDF name="user_sshkey" label="Public Key for Non-Root User" default="" />
+#
+# SUDO CONFIG
+#
+# <UDF name="sudo_usergroup" label="Usergroup to use for Sudo Accounts" default="sudo" />
+# <UDF name="sudo_passwordless" label="Passwordless Sudo" oneof="Require Password,Do Not Require Password", default="Require Password" />
+#
+# SSH CONFIG
+#
+# <UDF name="sshd_port" label="SSH Port" default="22" />
+# <UDF name="sshd_permitroot" label="SSH Permit Root Login" oneOf="No,Yes" default="No" />
+# <UDF name="sshd_passwordauth" label="SSH Password Authentication" oneOf="No,Yes" default="No" />
+#
+# FIREWALL CONFIG
+#
+# <UDF name="allowedServices" label="Allowed services" example="Punch holes in the firewall for these services. SSH is already open on port 22 (see Restrict SSH)." default="" manyOf="FTP Server: TCP 21,Telnet: TCP 23,SMTP: TCP 25,DNS Server: TCP/UDP 53,Web Server: TCP 80,POP3 Mail Service: TCP 110,NTP Service: UDP 123,IMAP Mail Service: TCP 143,SSL Web Server: TCP 443,Mail Submission: TCP 587,SSL IMAP Server: TCP 993,OpenVPN Server: UDP 1194,IRC Server: TCP 6667" />
+# <UDF name="extraTCP" label="Extra TCP holes (SSH is already handled for you)" example="Extra holes in the firewall for TCP. Understands service names ('kerberos') and port numbers ('31337'), separate by spaces." default="" />
+# <UDF name="extraUDP" label="Extra UDP holes" example="Extra holes in the firewall for UDP. Understands service names ('daytime') and port numbers ('1094'), separate by spaces." default="" />
+#
+
+logfile="/var/log/stackscript.log"
+
+#  System Update & Install Packages
+
+systemUpdate() {
+  echo "" >> $logfile
+  echo "Performing System Update..." >> $logfile
+
+  if [ -f /etc/apt/sources.list ]; then
+	  sudo apt-get update -qqy
+		sudo apt-get upgrade -qqy
+  else
+     echo "Your distribution is not supported by this StackScript"
+     exit
+  fi
+}
+
+installDeps() {
+  echo "" >> $logfile
+  echo "Installing the following basic applications:" >> $logfile
+  echo "sudo ufw" >> /var/log/stackscript.log
+
+  if [ -f /etc/apt/sources.list ]; then
+    sudo apt -qqy install sudo ufw
+  else
+     echo "Your distribution is not supported by this StackScript"
+     exit
+  fi
+}
+
+# Firewall Security
+
+setupFirewall() {
+  echo "" >> $logfile
+  echo "Configure UFW..." >> $logfile
+
+  sudo ufw allow $SSHD_PORT/tcp
+
+  IFS=$','
+  for service in $ALLOWEDSERVICES; do
+    echo Service: $service
+    interested=${service#*: }
+    IFS=$' '
+    set -- $interested
+    for i in TCP UDP; do
+      if [[ "$1" == *$i* ]]; then
+        PP=$(echo $2/$i | tr '[:upper:]' '[:lower:]')
+        sudo ufw allow $PP
+      fi
+    done
+  done
+  unset IFS
+
+  # Extras
+  for i in $EXTRAUDP; do
+      echo Allowing: UDP $i
+      sudo ufw allow $i/udp
+  done
+  for i in $EXTRATCP; do
+      echo Allowing: TCP $i
+      sudo ufw allow $i/tcp
+  done
+
+  sudo ufw enable
+  echo "Completed UFW config..." >> $logfile
+}
+
+# SSH Security
+
+setupSSH() {
+  echo "" >> $logfile
+  echo "Beginning SSH security setup..." >> $logfile
+
+  cp /etc/sudoers /etc/sudoers.tmp
+  chmod 0640 /etc/sudoers.tmp
+  test "${SUDO_PASSWORDLESS}" == "Do Not Require Password" && (echo "%`echo ${SUDO_USERGROUP} | tr '[:upper:]' '[:lower:]'` ALL = NOPASSWD: ALL" >> /etc/sudoers.tmp)
+  test "${SUDO_PASSWORDLESS}" == "Require Password" && (echo "%`echo ${SUDO_USERGROUP} | tr '[:upper:]' '[:lower:]'` ALL = (ALL) ALL" >> /etc/sudoers.tmp)
+  chmod 0440 /etc/sudoers.tmp
+  mv /etc/sudoers.tmp /etc/sudoers
+
+  # Configure SSHD
+  echo "Port ${SSHD_PORT}" > /etc/ssh/sshd_config.tmp
+
+  sed -n 's/\(HostKey .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(UsePrivilegeSeparation .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(KeyRegenerationInterval .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(ServerKeyBits .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(SyslogFacility .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(LogLevel .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(LoginGraceTime .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  echo "PermitRootLogin `echo ${SSHD_PERMITROOT} | tr '[:upper:]' '[:lower:]'`" >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(StrictModes .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(RSAAuthentication .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(PubkeyAuthentication .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(IgnoreRhosts .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(RhostsRSAAuthentication .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(HostbasedAuthentication .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(PermitEmptyPasswords .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(ChallengeResponseAuthentication .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  echo "PasswordAuthentication `echo ${SSHD_PASSWORDAUTH} | tr '[:upper:]' '[:lower:]'`" >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(X11Forwarding .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(X11DisplayOffset .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(PrintMotd .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(PrintLastLog .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(TCPKeepAlive .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(MaxStartups .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(AcceptEnv .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(Subsystem .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+  sed -n 's/\(UsePAM .*\)/\1/p' < /etc/ssh/sshd_config >> /etc/ssh/sshd_config.tmp
+
+  chmod 0600 /etc/ssh/sshd_config.tmp
+  mv /etc/ssh/sshd_config.tmp /etc/ssh/sshd_config
+
+  service ssh restart
+  echo "Completed SSH security setup..." >> $logfile
+}
+
+# User Creation
+
+createUser() {
+  echo "" >> $logfile
+  echo "Beginning User setup..." >> $logfile
+
+  # Create User & Add SSH Key
+  USER_NAME_LOWER=`echo ${USER_NAME} | tr '[:upper:]' '[:lower:]'`
+
+  useradd -m -s /bin/bash -G sudo ${USER_NAME_LOWER}
+  echo "${USER_NAME_LOWER}:${USER_PASSWORD}" | chpasswd
+
+  USER_HOME=`sed -n "s/${USER_NAME_LOWER}:x:[0-9]*:[0-9]*:[^:]*:\(.*\):.*/\1/p" < /etc/passwd`
+
+  sudo -u ${USER_NAME_LOWER} mkdir ${USER_HOME}/.ssh
+  echo "${USER_SSHKEY}" >> $USER_HOME/.ssh/authorized_keys
+  chmod 0600 $USER_HOME/.ssh/authorized_keys
+  chown ${USER_NAME_LOWER}:${USER_NAME_LOWER} $USER_HOME/.ssh/authorized_keys
+
+  echo "Completed User setup..." >> $logfile
+}
+
+# Run Install in Sequence
+
+# starting of stackscript
+touch $logfile
+echo "Starting StackScript processing..." >> $logfile
+
+systemUpdate
+
+installDeps
+
+setupFirewall
+
+setupSSH
+
+createUser
+
+echo "StackScript Done..." >> $logfile
